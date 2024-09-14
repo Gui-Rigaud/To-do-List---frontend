717b2e829cb2f7343da9dae196317503cbe15658
Gui-Rigaud <guirigaud@hotmail.com>
2024-09-14 16:22:34 -0300

    Implementando a tela de Login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { AuthProvider } from '../contexts/AuthContext';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 import '../styles/global.css'
- 
+import Head from 'next/head';
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <AuthProvider>
+        <Component {...pageProps} />
+        <ToastContainer autoClose={3000} />
+      </AuthProvider>
+    </>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,94 @@
-export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+import { useContext, FormEvent, useState } from 'react'
+
+import Head from "next/head"
+import styles from '../styles/home.module.scss';
+
+import { Input } from "../pages/components/UI/Input";
+import { Button } from "../pages/components/UI/Button";
+import Link from "next/link";
+
+import { GetServerSideProps } from 'next';
+
+import { AuthContext } from "../contexts/AuthContext";
+import { toast } from 'react-toastify';
+import { canSSRGuest } from '../utils/canSSRGuest';
+
+
+export default function Home() {
+
+  const { signIn } = useContext(AuthContext);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [loading, setLoading] = useState(false);
+
+  async function handleLogin(event: FormEvent) {
+      
+    event.preventDefault();
+
+    if(email === '' || password === ''){
+      toast.error("Preencha todos os campos!", { theme: "dark" });
+      return;
+    }
+
+    setLoading(true);
+  
+    let data = {
+      email,
+      password
+    }
+  
+    await signIn(data);
+
+    setLoading(false);
   }
+
+  return (
+    <>
+      <Head>
+        <title>To-do List - Faça seu login</title>
+      </Head>
+
+      <div className={styles.containerCenter}>
+
+        <div className={styles.login}>
+        <h1 style={{color: "#fff"}}>Login</h1>
+          <form onSubmit={handleLogin}>
+            <Input
+              type='text'
+              placeholder="Digite seu email"
+              value={email}
+              onChange={ (e) => setEmail(e.target.value)}
+            />
+
+            <Input
+              type='password'
+              placeholder="Sua senha"
+              value={password}
+              onChange={ (e) => setPassword(e.target.value)}
+            />
+
+            <Button
+              type="submit"
+              loading={loading}
+            >
+              Acessar
+            </Button>
+          </form>
+
+          <Link href="/signup" legacyBehavior>
+            <a className={styles.text}>Não possui uma conta? Cadastre-se</a>
+          </Link>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export const getServerSideProps = canSSRGuest(async (ctx) => {
+  
+  return {
+    props: {}
+  }
+})
